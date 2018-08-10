d17cc92b316f4d7f043b3b22314e451f4d702682
Maanit Madan <me@maanit.me>
2018-08-10 05:57:25 +0000

    changes

--- demo.py
+++ demo.py
@@ -3,14 +3,14 @@ import serial
 # from picamera import PiCamera
 from time import sleep
 from gtts import gTTS
-
+import os
 # camera = PiCamera()
 
 arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
 
-tts = gTTS(text="Hi. I am Bippy. Are you ready to start summer reading?", lang='en')
-tts.save("demoText.mp3")
-os.system("mpg321 demoText.mp3")
+#tts = gTTS(text="Hi. I am Bippy. Are you ready to start summer reading?", lang='en')
+#tts.save("demoText.mp3")
+#os.system("mpg321 demoText.mp3")
 
 while True:
     data = arduino.read()
