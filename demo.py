3a7478771175fbdf4f9fc663555bb54a952b758d
Maanit Madan <me@maanit.me>
2018-08-10 08:59:07 -0400

    changed port

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import serial
 from gtts import gTTS
 import os
 
-arduino = serial.Serial(port='/dev/cu.wchusbserial1410', baudrate=9600)
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
 
 tts = gTTS(text="Hi. I am Bippy. Are you ready to start summer reading?", lang='en')
 tts.save("demoText.mp3")
