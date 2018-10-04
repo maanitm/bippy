d8ab50a25e728667d031d6530b2f9865bdf51f54
Maanit Madan <me@maanit.me>
2018-10-04 18:36:54 -0400

    arduino motor update

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,8 +1,13 @@
-int redPin = 2;
-int greenPin = 3;
-int bluePin = 4;
+int redPin = 8;
+int greenPin = 7;
+int bluePin = 6;
 
-int buttonPin = 5;
+int motorPin1 = 2;
+int motorPin2 = 3;
+int motorPin3 = 4;
+int motorPin4 = 5;
+
+int buttonPin = 9;
 
 int red = 0;
 int green = 0;
@@ -13,6 +18,11 @@ bool runState = false;
 
 int buttonStartTime = 0;
 bool buttonStarted = false;
+
+int halfStep = 8;
+
+int stepCount = 0;
+int stepDelay = 1;
 
 int startTime = 0;
 
@@ -27,6 +37,11 @@ void setup() {
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT); 
   
   pinMode(buttonPin, INPUT);
 }
@@ -36,6 +51,11 @@ void loop() {
   int state = digitalRead(buttonPin);
   double elapsedTime = double(millis()-startTime);
 
+  if (stepCount < 500) {
+    moveSteps(500);
+    stepCount += 500;  
+  }
+  
   if (runState == 1 && elapsedTime <= totalTime) {
     double diff = elapsedTime/totalTime;
     double incr = 255*diff;
@@ -101,6 +121,70 @@ void loop() {
   setColor(red, green, blue);
   delay(100);
 }
+
+void moveSteps(int steps) {
+  for(int x = 0; x < steps; x++) {
+  for(int i = 0; i < 8; i++) {
+    switch(i){ 
+   case 0: 
+     digitalWrite(motorPin1, LOW);  
+     digitalWrite(motorPin2, LOW); 
+     digitalWrite(motorPin3, LOW); 
+     digitalWrite(motorPin4, HIGH); 
+   break;  
+   case 1: 
+     digitalWrite(motorPin1, LOW);  
+     digitalWrite(motorPin2, LOW); 
+     digitalWrite(motorPin3, HIGH); 
+     digitalWrite(motorPin4, HIGH); 
+   break;  
+   case 2: 
+     digitalWrite(motorPin1, LOW);  
+     digitalWrite(motorPin2, LOW); 
+     digitalWrite(motorPin3, HIGH); 
+     digitalWrite(motorPin4, LOW); 
+   break;  
+   case 3: 
+     digitalWrite(motorPin1, LOW);  
+     digitalWrite(motorPin2, HIGH); 
+     digitalWrite(motorPin3, HIGH); 
+     digitalWrite(motorPin4, LOW); 
+   break;  
+   case 4: 
+     digitalWrite(motorPin1, LOW);  
+     digitalWrite(motorPin2, HIGH); 
+     digitalWrite(motorPin3, LOW); 
+     digitalWrite(motorPin4, LOW); 
+   break;  
+   case 5: 
+     digitalWrite(motorPin1, HIGH);  
+     digitalWrite(motorPin2, HIGH); 
+     digitalWrite(motorPin3, LOW); 
+     digitalWrite(motorPin4, LOW); 
+   break;  
+     case 6: 
+     digitalWrite(motorPin1, HIGH);  
+     digitalWrite(motorPin2, LOW); 
+     digitalWrite(motorPin3, LOW); 
+     digitalWrite(motorPin4, LOW); 
+   break;  
+   case 7: 
+     digitalWrite(motorPin1, HIGH);  
+     digitalWrite(motorPin2, LOW); 
+     digitalWrite(motorPin3, LOW); 
+     digitalWrite(motorPin4, HIGH); 
+   break;  
+   default: 
+     digitalWrite(motorPin1, LOW);  
+     digitalWrite(motorPin2, LOW); 
+     digitalWrite(motorPin3, LOW); 
+     digitalWrite(motorPin4, LOW); 
+   break;  
+ }
+    delay(stepDelay);
+  }   
+  }
+}
  
 void setColor (int red, int green, int blue) {
   analogWrite(redPin, red);
@@ -127,5 +211,3 @@ void cameraClicked () {
   currentTask += 1;
   setColor(red, green, blue);
 }
-
-
