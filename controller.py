aaa13156e3a060e768942ee3cedd0b70d08d78ce
Maanit Madan <me@maanit.me>
2018-12-19 11:56:01 -0500

    take picture test

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 from threading import Thread
 import serial
-# from picamera import PiCamera
+from picamera import PiCamera
 from time import sleep
 from gtts import gTTS
 import os
@@ -10,6 +10,7 @@ import os
 
 # Constants
 arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+camera = PiCamera()
 
 # Variables
 connected = True
@@ -27,6 +28,20 @@ def sayText(text):
     os.system("mpg123 tempText.mp3")
     talking = False
 
+def takePicture():
+    arduino.write('r255;g255;b255')
+    sleep(0.5)
+    arduino.write('r000;g000;b000')
+    sleep(0.5)
+    arduino.write('r255;g255;b255')
+    sleep(0.5)
+    arduino.write('r000;g000;b000')
+    sleep(0.5)
+    arduino.write('r255;g255;b255')
+    camera.capture('/home/pi/Desktop/image.jpg')
+    sleep(0.5)
+    arduino.write('r000;g000;b000')
+
 # Main Thread
 startedActivity = False
 currentTask = -1
@@ -41,7 +56,9 @@ textsToSpeak = [
     "Wonderful! Let's move on to the next task. We are going to do %s now. Show me whenever you are ready",
     "Amazing! Let's take a short break now! I'll tell you when to start again.",
     "Are you sure that is the correct answer?",
-    "Great job! We are all done. Thank you, %s!" % name
+    "Great job! We are all done. Thank you, %s!" % name,
+    "Goodbye!",
+    "Please add a task to the app."
 ]
 while True:
     if connected:
@@ -53,15 +70,15 @@ while True:
                 currentTask = 0
                 startedActivity = True
             else:
-                sayText("Please add a task to the app.")
+                sayText(textsToSpeak[11])
             ioValue = "."
         if ioValue in "0":
-            sayText("Goodbye!")
+            sayText(textsToSpeak[10])
             ioValue = "."
             startedActivity = False
         if  ioValue in "2" and startedActivity:
             sayText(textsToSpeak[5])
-            sleep(3)
+            takePicture()
             if len(tasks) > currentTask + 1:
                 currentTask += 1
                 sayText(textsToSpeak[6] % tasks[currentTask]["name"])
